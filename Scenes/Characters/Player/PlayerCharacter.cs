c5523907e6d591f32153bd0d51d819078cbc63da
Andrei <andreip0pa44@gmail.com>
2025-10-02 01:23:29 +0200

    Remove unused methods

--- Scenes/Characters/Player/PlayerCharacter.cs
+++ Scenes/Characters/Player/PlayerCharacter.cs
@@ -9,24 +9,12 @@ public partial class PlayerCharacter: Node3D
 	public Node attackTarget;
 	[Export]
 	public HealthComponent healthComponent;
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-
 	public void Attack()
 	{
 		var healthComponent = attackTarget.GetChildren().Where(x => x is HealthComponent).First() as HealthComponent;
 		if (healthComponent != null)
-			healthComponent.TakeDamage(10);
+			healthComponent.TakeDamage(10);//TODO: Damage component 
 	}
-
 	public void GetAttacked()
 	{
 		healthComponent.TakeDamage(10);
--- Scenes/UI/HealthBar/HealthBar.cs
+++ Scenes/UI/HealthBar/HealthBar.cs
@@ -1,19 +1,15 @@
 using Godot;
-using System;
-
 public partial class HealthBar: Node3D
 {
 	[Export]
 	public HealthComponent healthComponent;
 	[Export]
 	public ProgressBar progressBar;
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		/*
+		TODO Make it signal based instead of in _Process
+		*/
 		progressBar.MaxValue = healthComponent.MaxHealth;
 		progressBar.Value = healthComponent.Health;
 	}
